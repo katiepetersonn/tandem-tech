565dde9732d15674d7a47e7e2a8764ce9ef40c66
katiepetersonn <katie.peterson19@gmail.com>
2017-06-11 14:58:12 +1000

    Comments CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_sessions
 
-  # before_action :lucky_number
   before_action :fetch_user
-    # Before any request in our system is handled, try and define @current_user (call fetch_user to do that)
 
   private
     def lucky_number
@@ -15,12 +13,5 @@ class ApplicationController < ActionController::Base
       @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
       session[:user_id] = nil unless @current_user.present?
 
-      # Check in the session variable
-        # If there is a key called :user_id
-          # Create an instance variable called @current_user that can be used on every page
-          # If you can't find a user with that ID
-            # Set session[:user_id] to nil
-        # If there isn't a key called user_id
-          # Do nothing
     end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,25 @@ class CommentsController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-    @comment = @item.comments.new(comment_params)
+    @comment = @user.comments.new(comment_params)
     @comment.user = @current_user
     @comment.save
 
     # raise "hell"
 
-    redirect_to item_path(@item)
+    redirect_to user_path(@user)
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
-    @comment = @item.comments.find(params[:id])
+    @user = User.find(params[:item_id])
+    @comment = @user.comments.find(params[:id])
     @comment.destroy
-    redirect_to item_path(@item)
+    redirect_to user_path(@user)
 
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:user_id, :post)
+    params.require(:comment).permit(:user_id, :body)
   end
 end
