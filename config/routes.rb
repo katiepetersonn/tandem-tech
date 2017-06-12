d48a796e6c15bc23b5fc605ffa2367c9f302f35e
katiepetersonn <katie.peterson19@gmail.com>
2017-06-12 22:15:08 +1000

    logout working

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
-  delete '/logout' => 'session#destroy'
+  get '/logout' => 'session#destroy'
 
 end
