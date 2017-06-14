5fa1a6d9bb18df007dd89e1d5e1e177a96682af4
katiepetersonn <katie.peterson19@gmail.com>
2017-06-14 18:55:28 +1000

    removed Sale scaffold

--- app/views/sales/_sale.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! sale, :id, :created_at, :updated_at
-json.url sale_url(sale, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :sales
   root to: "pages#home"
 
   resources :bookings
